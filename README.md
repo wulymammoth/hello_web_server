0cd678dce7f6210e954ca1811c727e89a4ed7ab8
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-19 17:06:59 -0700

    WIP

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Rust Programming Language | final project | web server
+
+## notes
+
+- ports
+  - `80` requires administrator privileges
+  - `> 1024` permits listening from non-administrators
+  - we also can't have two instances of our program running on the same port
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::prelude::*; // gain access to certain traits that let us read from and write to the stream
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    // port chosen for two reasons:
+    // 1. HTTP normally accepted
+    // 2. 7878 is "rust typed" on a telephone
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+// NOTE: the stream is mutable (even if we're just reading) because internal state may change
+// - it may actually read more data than we asked for
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
