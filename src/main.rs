b8c7190929bca336d0f7278cf91fb267309eeacd
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-22 14:26:41 -0700

    return HTML

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*; // gain access to certain traits that let us read from and write to the stream
 use std::net::{TcpListener, TcpStream};
 
@@ -6,8 +7,10 @@ fn main() {
     // 1. HTTP normally accepted
     // 2. 7878 is "rust typed" on a telephone
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+
         handle_connection(stream);
     }
 }
@@ -15,7 +18,28 @@ fn main() {
 // NOTE: the stream is mutable (even if we're just reading) because internal state may change
 // - it may actually read more data than we asked for
 fn handle_connection(mut stream: TcpStream) {
+    // declare a buffer that is 1024 bytes in size
     let mut buffer = [0; 1024];
+
+    // read the bytes and stick them into the mutable buffer
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    // the following converts the bytes in the buffer into a string
+    // the "lossy" part indicates it'll replace any invalid UTF-8 sequence with
+    // a ? symbol (U+FFFD REPLACEMENT CHARACTER)
+    // `let out = String::from_utf8_lossy(&buffer[..]);`
+    // `println!("Request: {}", out);`
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    // convert string to bytes and write to stream
+    // because the write operation could fail, we use unwrap
+    stream.write(response.as_bytes()).unwrap();
+
+    stream.flush().unwrap();
 }
